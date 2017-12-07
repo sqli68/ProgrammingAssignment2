01acb1b1fec82ae37533be22b2393618184f4088
Shuqin Li-Kokko <sqli68@hotmail.com>
2017-12-07 19:29:21 +0200

    Remove the empty line  before the function

--- cachematrix.R
+++ cachematrix.R
@@ -8,7 +8,6 @@
 ## 2. get the value of the matrix
 ## 3. set the value of inverse of the matrix
 ## 4. get the value of inverse of the matrix
-
 makeCacheMatrix <- function(x = numeric()){
   invs <- NULL
   set <- function(y) {
